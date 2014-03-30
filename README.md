ad6766e9979a761a212005e4c887b25181b8651f
Henrik Steen <henrist@henrist.net>
2014-03-30 17:42:22 +0200

    Div endringer og noen nye tegninger.

--- README.md
+++ README.md
@@ -26,23 +26,40 @@ Bokdata:
 ```
 
 Utlånsdata:
-TODO
+```
+{
+	kode: "strekkode for oppføringen til boka",
+	user: "brukernavn for den som har gjort utlånet",
+	startTime: "tidspunkt ved registrering",
+	endTime: "tidspunkt ved registrert innlevering, hvis innlevert"
+}
+```
 
 Use cases:
-* Skal kunne registrere bøker ved hjelp av ISBN-kode
-* Skal kunne registrere bøker manuelt ved inntasting av felter
-* Skal kunne tilordne en registrering mot en egen strekkode
-* Skal kunne oppdatere informasjon om bøker, samt markere dem som slettet hvis de kastes
-* Skal kunne søke opp bøker ved å søke på tittel og/eller forfatter
-* Brukere av biblioteket skal kunne registrere utlån (f.eks. ved hjelp av brukersystemet)
-* Brukere skal kunne registrere innlevering av bøker
+* En administrator skal kunne registrere bøker ved hjelp av ISBN-kode og/eller manuelt inntasting av felter
+* En administrator skal kunne tilordne en registrering mot en egen strekkode
+* En administrator skal kunne oppdatere informasjon om bøker (alle feltene), samt markere dem som slettet med begrunnelse hvis de kastes/har forsvunnet
+* En bruker skal kunne søke opp bøker ved å søke på tittel og/eller forfatter
+* En bruker skal kunne vise liste over bøker i de ulike kategoriene
+* En bruker skal kunne registrere utlån av bøker
+* En bruker skal kunne registrere innlevering av bøker
+
+### Registrering av nye bøker
+![Flyt for registrering av nye bøker](docs/ny-registrering-flyt.png)
+
+### Registrering av utlån
+![Flyt for registrering av utlån](docs/utlaan-flyt.png)
 
 ## Strekkoder
 Hver bok på biblioteket får sin egen oppføring i boksystemet. For å enkelt skille mellom bøker får alle bøkene sin egen stekkode med eget nummer. Dette kan brukes for å kjapt identifisere riktig oppføring, samt holde orden på hvilke bøker som er registrert i systemet.
 
-Forslag til format: BSXXXXXX hvor XXXXXX er løpenummer som starter på 000001.
+For å unngå at man søker opp tilfeldige bøker og registrerer utlån, kan det være en fordel å ha et par kontrollsifre. Disse kan ha tilfeldig verdi mellom 0 og 255 og kodes mellom 00 og FF.
 
-Strekkodeformat: Code 39 mod 43
+Forslag til format: BS-XXXX-KK hvor XXXXX er løpenummer i HEX som starter på 00001 og KK er kontrollsifre som nevnt ovenfor.
+
+Løpenummeret skal være unikt blant alle registreringene.
+
+Strekkodeformat: Code 128
 
 ## Andre ideer
 * Ha et felt som holder orden på når bøker sist ble sjekket (scannet)? Da blir det mulig å finne ut hvilke bøker som mangler.
--- eksempel.py
+++ eksempel.py
@@ -1,13 +1,34 @@
 # -*- coding: utf-8 -*-
 import cmd
-import json
-import urllib2
+#import json
+#import urllib2
+#import pprint
+from collections import OrderedDict
+from apiclient.discovery import build
 from pymongo import MongoClient
 
+# sett opp MongoDB-tilkobling
+# foreløpig ingen pålogging i prototypen..
 client = MongoClient()
-
 database = client['biblioteket']
 boker = database['boker']
+
+# hent inn API-nøkkel til Google Books
+# denne filen skal kun inneholde nøkkelen
+with open('api_key') as apifile:
+	key = apifile.read().strip()
+
+# sett opp Google-tjeneste for søking
+service = build('books', 'v1', developerKey=key)
+
+def sokISBN(isbn):
+	request = service.volumes().list(q=('isbn:%s' % isbn))
+	response = request.execute()
+
+	try:
+		return response['items'][0]['volumeInfo']
+	except (NameError, KeyError, IndexError):
+		return None
 
 
 class Console(cmd.Cmd):
@@ -28,6 +49,21 @@ class Console(cmd.Cmd):
 		for bok in boker.find():
 			print " * %s (%s)" % (bok['title'], bok['publishedDate'])
 
+	def do_fjern(self, args):
+		s = raw_input("Scan ISBN: ")
+
+		res = boker.find({"industryIdentifiers.identifier": s})
+
+		if res.count() == 0:
+			print "Fant ikke boka!"
+			return
+
+		for x in res:
+			boker.remove(x['_id'])
+
+		print "Boka ble fjernet"
+
+
 	def do_EOF(self, args):
 		print
 		return -1
@@ -39,15 +75,12 @@ class Console(cmd.Cmd):
 		registrer = Registrer()
 		registrer.cmdloop()
 
-	def do_exit(self, args):
-		return -1
-
 
 class Registrer(cmd.Cmd):
 	def __init__(self):
 		cmd.Cmd.__init__(self)
 		self.prompt = ": "
-		self.intro = "Scan ISBN-kode!"
+		self.intro = "Scan eller skriv inn ISBN-kode!"
 
 	def do_exit(self, args):
 		return -1
@@ -59,19 +92,64 @@ class Registrer(cmd.Cmd):
 	def default(self, line):
 		print "Du søker etter: %s" % line
 
-		data = urllib2.urlopen("https://www.googleapis.com/books/v1/volumes?q=isbn:%s" % line).read()
+		obj = sokISBN(line)
+		if not obj:
+			print "Ingen treff ble funnet."
+			return
 
-		obj = json.loads(data)
-		obj = obj['items'][0]['volumeInfo']
+		newobj = {}
+		newobj['gdata'] = obj
+		
+		for x in ('title', 'subtitle', 'authors', 'publishedDate', 'description', 'industryIdentifiers', 'pageCount', 'categories'):
+			if x in obj:
+				newobj[x] = obj[x]
 
-		id = boker.insert(obj)
-		elm = boker.find_one(id)
+		print "Du er i ferd med å registrere følgende bok:"
+		
+		texts = OrderedDict([
+			("title", "Tittel"),
+			("subtitle", "Undertittel"),
+			("authors", "Forfattere"),
+			("publishedDate", "Publiseringsdato"),
+			("description", "Beskrivelse"),
+			("industryIdentifiers", "ISBN-kode(r)"),
+			("pageCount", "Sideantall"),
+			("categories", "Kategorier")])
 
-		print "Du har registrert følgende bok:"
-		print "Tittel: %s" % elm['title']
-		print "Publisert: %s" % elm['publishedDate']
+		# list opp alle felter
+		i = 1
+		for key, value in texts.iteritems():
+			if not key in newobj:
+				v = "*Ingen verdi*"
+			else:
+				v = newobj[key]
+				if key == "authors" or key == "categories":
+					v = "\n  %s" % "\n  ".join([("* %s" % a) for a in v])
+				if key == "industryIdentifiers":
+					v = "\n  %s" % "\n  ".join([("* %s (%s)" % (a['identifier'], a['type'])) for a in v])
+			print "[%d] %s: %s" % (i, value, v)
+			i+=1
 
-		#return -1
+		# sjekk om boka allerede er registrert
+		if "industryIdentifiers" in newobj:
+			search = []
+			for x in newobj['industryIdentifiers']:
+				search.append({"industryIdentifiers.identifier": x['identifier']})
+			q = {"$or": search}
+			res = boker.find(q)
+			if res.count() > 0:
+				print "Vi har allerede en bok med samme ISBN!"
+				s = raw_input('Ønsker du å fortsette? [J/n]: ')
+				if not (s == "" or s == "j" or s == "J"):
+					print "Avbryter.."
+					return
+
+		#id = 
+		boker.insert(newobj)
+		#elm = boker.find_one(id)
+
+		print "Boka ble lagt til"
+		return -1
 
 if __name__ == "__main__":
 	console = Console()
